d440cc36fde76e29b572218d85bcc606ee5e9045
CarlosEduardoL <carlos.lizalda1@hotmail.com>
2019-05-17 19:32:45 -0500

    Al registrar o actualizar un usuario se le manda un correo con su QR

--- FrontEnd/GymMannager/src/main/java/controller/MainViewController.java
+++ FrontEnd/GymMannager/src/main/java/controller/MainViewController.java
@@ -9,18 +9,27 @@ import com.google.zxing.qrcode.QRCodeWriter;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.User;
 
+import javax.activation.DataHandler;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
@@ -38,27 +47,79 @@ public class MainViewController implements Initializable {
     }
 
     public void generateQR(ActionEvent event) throws WriterException, IOException {
+        InputStream inputStream = getQRStream();
+        qr.setImage(new Image(inputStream));
+    }
+
+    private InputStream getQRStream() throws WriterException, IOException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = qrCodeWriter.encode(id.getText(), BarcodeFormat.QR_CODE, 300, 300);
 
         ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
 
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(pngOutputStream.toByteArray());
+        return new ByteArrayInputStream(pngOutputStream.toByteArray());
+    }
 
-        qr.setImage(new Image(inputStream));
+    private void modifyUser(String add) throws IOException {
+        User user = new User(name.getText(), surname.getText(), email.getText(), id.getText(), getDate(init)
+                , getDate(end), Double.parseDouble(value.getText()));
+        String response = request(new Gson().toJson(user), add);
+        sendQR(email.getText());
     }
 
     public void addUser(ActionEvent event) throws IOException {
-        User user = new User(name.getText(),surname.getText(),email.getText(),id.getText(),getDate(init)
-                ,getDate(end),Double.parseDouble(value.getText()));
-        String response = request(new Gson().toJson(user),"add");
+        modifyUser("add");
     }
 
     public void update(ActionEvent event) throws IOException {
-        User user = new User(name.getText(),surname.getText(),email.getText(),id.getText(),getDate(init)
-                ,getDate(end),Double.parseDouble(value.getText()));
-        String response = request(new Gson().toJson(user),"update");
+        modifyUser("update");
+    }
+
+    private void sendQR(String userEmail) {
+        if(!userEmail.contains("@") || userEmail.contains(" ") || !userEmail.contains(".")) emailError();
+        else {
+            Properties props = new Properties();
+            // Nombre del host de correo, es smtp.gmail.com
+            props.setProperty("mail.smtp.host", "smtp.gmail.com");
+            // TLS si está disponible
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            // Puerto de gmail para envio de correos
+            props.setProperty("mail.smtp.port","587");
+            // Nombre del usuario
+            props.setProperty("mail.smtp.user", "gymprograred2019@gmail.com");
+            // Si requiere o no usuario y password para conectarse.
+            props.setProperty("mail.smtp.auth", "true");
+            Session session = Session.getDefaultInstance(props);
+            session.setDebug(true);
+            BodyPart texto = new MimeBodyPart();
+            try {
+                texto.setText("Con este QR podra entrar al gym");
+                BodyPart adjunto = new MimeBodyPart();
+                adjunto.setDataHandler(new DataHandler(new ByteArrayDataSource(getQRStream(),"image/jpeg")));
+                MimeMultipart multiParte = new MimeMultipart();
+                multiParte.addBodyPart(texto);
+                multiParte.addBodyPart(adjunto);
+                MimeMessage message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("gymprograred2019@gmail.com"));
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(userEmail));
+                message.setSubject("Gym QR");
+                message.setContent(multiParte);
+                Transport t = session.getTransport("smtp");
+                t.connect("gymprograred2019@gmail.com","prograred-2019");
+                t.sendMessage(message,message.getAllRecipients());
+                t.close();
+            } catch (MessagingException | IOException | WriterException e) {
+                emailError();
+            }
+        }
+    }
+
+    private void emailError() {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+        errorAlert.setHeaderText("Email not valid");
+        errorAlert.setContentText("The email is invalid, message not sent");
+        errorAlert.showAndWait();
     }
 
     private java.util.Date getDate(DatePicker picker) {
